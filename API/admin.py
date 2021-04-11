878206e9fb35fad59820bcbcd4fd99309a213ab2
ziaurjoy <ziaurjoy802@gmail.com>
2021-04-12 01:51:30 +0600

    Model ready

---
+++ API/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Service)
+admin.site.register(ClientReviews)
+admin.site.register(ProjectTable)
+admin.site.register(Contact)
+admin.site.register(Footer)
+admin.site.register(HomeETC)
+admin.site.register(ChartData)
+admin.site.register(Courses)
